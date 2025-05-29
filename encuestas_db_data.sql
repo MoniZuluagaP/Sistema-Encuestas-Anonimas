2a35744f80971de14174a4bab892d05c756b24ad
MoniZuluagaP <monicazul.desarrollo@gmail.com>
2025-05-29 20:16:33 -0300

    Update encuestas_db_data.sql

--- encuestas_db_data.sql
+++ encuestas_db_data.sql
@@ -1,23 +1,23 @@
 INSERT INTO "public"."encuesta" ("id", "nombre", "fecha_vencimiento", "email", "codigo_respuesta", "codigo_resultados") VALUES
-(8, 'Satisfacción del Servicio al Cliente', '2025-07-01 00:00:00', 'cliente1@mail.com', '7e0a33d1-379c-4852-93c5-22b18f6284d6', '3b7763f2-3449-4fa3-87c5-7a74795a4b7f');
+(1, 'Satisfacción del Servicio al Cliente', '2025-07-01 00:00:00', 'cliente1@mail.com', 'e49eb8c9-78d5-4314-b7d4-c72d194bc27f', '1165ea20-cd06-4ddf-a678-3658976e3fc9');
 
 INSERT INTO "public"."pregunta" ("id", "numero", "texto", "tipo", "encuestaId") VALUES
-(10, 1, '¿Cómo calificarías nuestro servicio?', 'OPCION_MULTIPLE_SELECCION_SIMPLE', 8),
-(11, 2, '¿Qué sugerencias tienes para mejorar?', 'ABIERTA', 8);
+(1, 1, '¿Cómo calificarías nuestro servicio?', 'OPCION_MULTIPLE_SELECCION_SIMPLE', 1),
+(2, 2, '¿Qué sugerencias tienes para mejorar?', 'ABIERTA', 1);
 
 INSERT INTO "public"."opcion" ("id", "texto", "numero", "preguntaId") VALUES
-(4, 'Excelente', 1, 10),
-(5, 'Bueno', 2, 10),
-(6, 'Regular', 3, 10),
-(7, 'Malo', 4, 10);
+(1, 'Excelente', 1, 1),
+(2, 'Bueno', 2, 1),
+(3, 'Regular', 3, 1),
+(4, 'Malo', 4, 1);
 
 INSERT INTO "public"."respuesta" ("id", "encuestaId") VALUES
-(2, 8);
+(1, 1);
 
 INSERT INTO "public"."respuesta_abierta" ("id", "texto", "preguntaId", "respuestaId") VALUES
-(4, 'Más personal para atención al cliente.', 11, 2);
+(1, 'Más personal para atención al cliente.', 2, 1);
 
 INSERT INTO "public"."respuesta_opcion" ("id", "respuestaId", "opcionId") VALUES
-(1, 2, 5);
+(1, 1, 3);
 
 
