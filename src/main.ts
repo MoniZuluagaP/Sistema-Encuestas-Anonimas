a5a879f834c8c2b22912e0bc53ce4f4c118530f1
MoniZuluagaP <monicazul.desarrollo@gmail.com>
2025-06-10 15:49:34 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
   app.useGlobalPipes (new ValidationPipe ({
     whitelist: true   //fuerza que se cumpla lo que hay en el DTO 
   }))
+
+  app.enableCors()
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
